88f3c89a800ad16e1362dc9076b4bccef63f1ed5
Vijaylaxmi Biradar <vijaylaxmipb@gmail.com>
2024-11-24 16:47:52 +0000

    added products app,models,catergories,sub catergories & fixtures

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Category, Product, Subcategory
 
-# Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'friendly_name')
+
+@admin.register(Subcategory)
+class SubcategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'friendly_name', 'category')
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'price', 'rating')
