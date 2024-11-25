7ae30f1b86ec66d20dab95aa5c85b2894ee734c1
Vijaylaxmi Biradar <vijaylaxmipb@gmail.com>
2024-11-25 15:18:03 +0000

    Customized admin

--- products/admin.py
+++ products/admin.py
@@ -3,7 +3,13 @@ from .models import Category, Product, Subcategory
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('name', 'friendly_name')
+    list_display = (
+
+        'friendly_name',
+        'name',
+    )
+
+   
 
 @admin.register(Subcategory)
 class SubcategoryAdmin(admin.ModelAdmin):
@@ -11,4 +17,13 @@ class SubcategoryAdmin(admin.ModelAdmin):
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('name', 'category', 'price', 'rating')
+    list_display = (
+        'sku',
+        'name',
+        'category',
+        'price',
+        'rating',
+        'image',
+    )
+    ordering = ('sku',)
+
