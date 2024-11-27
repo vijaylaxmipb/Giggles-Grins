4414935d6e2a94918a627105ad5a6e2c37f7b00b
Vijaylaxmi Biradar <vijaylaxmipb@gmail.com>
2024-11-27 14:54:04 +0000

    added & updated category & subcategory section

--- products/urls.py
+++ products/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.all_products, name='products'),
     path('<int:product_id>/', views.product_detail, name='product_detail'),
+    path('category/<str:category_name>/', views.products_by_category, name='products_by_category'),
+    path('subcategory/<str:subcategory_name>/', views.products_by_subcategory, name='products_by_subcategory'),
+
 ]
