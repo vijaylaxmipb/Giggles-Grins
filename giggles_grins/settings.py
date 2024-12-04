1c061c216914c6d84f32561108392a4816f8ccb1
Vijaylaxmi Biradar <vijaylaxmipb@gmail.com>
2024-12-04 13:52:30 +0000

    add to bag functionalityto bag

--- giggles_grins/settings.py
+++ giggles_grins/settings.py
@@ -79,8 +79,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',# required by allauth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'django.template.context_processors.request',
                 'products.context_processors.categories_with_subcategories',
+                'bag.contexts.bag_contents',
             ],
         },
     },
@@ -161,6 +161,9 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+FREE_DELIVERY_THRESHOLD = 50
+STANDARD_DELIVERY_PERCENTAGE = 10
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
