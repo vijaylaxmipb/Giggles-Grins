589ddd6825088bca1e9a1f6a7abbf71a896ddd63
Vijaylaxmi Biradar <vijaylaxmipb@gmail.com>
2024-12-05 16:29:49 +0000

    added review function

--- giggles_grins/urls.py
+++ giggles_grins/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,6 +25,7 @@ urlpatterns = [
     path('', include('home.urls')),
     path('bag/', include('bag.urls')),
     path('products/', include('products.urls')),
+    path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
     
     
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
