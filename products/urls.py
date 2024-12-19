d1c45675f1051c5cc371909d335352f74b5701fe
Vijaylaxmi Biradar <vijaylaxmipb@gmail.com>
2024-12-19 16:01:12 +0000

    Added deleting product functionality to the product folder

--- products/urls.py
+++ products/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('subcategory/<str:subcategory_name>/', views.products_by_subcategory, name='products_by_subcategory'),
     path('add/', views.add_product, name='add_product'),
     path('edit/<int:product_id>/', views.edit_product, name='edit_product'),
+    path('delete/<int:product_id>/', views.delete_product, name='delete_product'),
     
 ]
