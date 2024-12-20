3defa607e1b992dcd9c627e01d312cbf09838b0b
Vijaylaxmi Biradar <vijaylaxmipb@gmail.com>
2024-12-20 17:34:14 +0000

    updated setting file

--- giggles_grins/settings.py
+++ giggles_grins/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', '')
+SECRET_KEY = 'tp&jc=054t@d1ep@8-iy=qzci01vf%j+$$fh6dk6(t4y!kh^c'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
