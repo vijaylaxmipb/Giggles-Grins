b45008fef92e55999f603ce4cc967530a0875fee
Vijaylaxmi Biradar <vijaylaxmipb@gmail.com>
2024-12-20 23:07:43 +0000

    heroku deployment

--- giggles_grins/settings.py
+++ giggles_grins/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'tp&jc=054t@d1ep@8-iy=qzci01vf%j+$$fh6dk6(t4y!kh^c'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
@@ -142,6 +142,7 @@ else:
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
             'NAME': BASE_DIR / 'db.sqlite3',
+            'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
         
     }
 }
