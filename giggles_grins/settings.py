25f2767b92a2d1de4c58f156fcae1fd762e5271e
Vijaylaxmi Biradar <vijaylaxmipb@gmail.com>
2024-12-24 15:18:55 +0000

    Updated setting file to fox error

--- giggles_grins/settings.py
+++ giggles_grins/settings.py
@@ -216,9 +216,9 @@ if 'USE_AWS' in os.environ:
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
     # Static and media files
-    STATICFILES_STORAGE = 'custom_storages.StaticStorage'
+    STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
     STATICFILES_LOCATION = 'static'
-    DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
+    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
     MEDIAFILES_LOCATION = 'media'
 
     # Override static and media URLs in production
