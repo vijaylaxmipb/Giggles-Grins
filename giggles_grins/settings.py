cfa89dffec4c7b6f9db8406e1e1886becbc4690f
Vijaylaxmi Biradar <vijaylaxmipb@gmail.com>
2025-01-01 00:32:51 +0000

    added base.css and login/register refactor

--- giggles_grins/settings.py
+++ giggles_grins/settings.py
@@ -201,8 +201,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),  # Path to your static files
 ]
-#STATIC_ROOT = BASE_DIR / "staticfiles"
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = BASE_DIR / "staticfiles"
+#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
 
