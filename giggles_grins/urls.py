c47e6c9df401721ae92cfe4c85bd8d22cf837ea4
Vijaylaxmi Biradar <vijaylaxmipb@gmail.com>
2025-01-03 11:55:39 +0000

    added 404 error file

--- giggles_grins/urls.py
+++ giggles_grins/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from .views import handler404
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -31,3 +32,5 @@ urlpatterns = [
 
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+handler404 = 'boutique_ado.views.handler404'
