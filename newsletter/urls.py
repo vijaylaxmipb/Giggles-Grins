50f06cffdfbb7eba141d86c1c326c6604b025fe4
Vijaylaxmi Biradar <vijaylaxmipb@gmail.com>
2025-01-29 23:10:27 +0000

    Fix: Add confirmation email system for newsletter signup

--- newsletter/urls.py
+++ newsletter/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('signup/', views.newsletter_signup, name='newsletter_signup'),
+    path('confirm/<str:email_b64>/<str:token>/', views.newsletter_confirm, name='newsletter_confirm'),
 ]
